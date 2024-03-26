1bd4adb55c56fa3b92373d6b1be8bb675d54a27a
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-03-26 21:45:37 +0200

    add inner unpaired markup check

--- src/main/java/org/example/MarkupChecker.java
+++ src/main/java/org/example/MarkupChecker.java
@@ -21,6 +21,9 @@ public class MarkupChecker {
     }
 
     public void checkUnpairedMarkup(String text) {
+        for (String markup: markups) {
+            checkInnerMarkup(text, markup);
+        }
         text = text.replaceAll(BOLD_REGEX, "BOLD");
         text = text.replaceAll(ITALIC_REGEX, "ITALIC");
         text = text.replaceAll(MONOSPACED_REGEX, "MONO");
@@ -35,6 +38,32 @@ public class MarkupChecker {
             if (word.startsWith(markup) && !word.endsWith(markup) ||
                     word.endsWith(markup) && !word.startsWith(markup)) {
                 throw new IllegalArgumentException("Error: unpaired markup");
+            }
+            hasInnerMarkup(word, markup);
+        }
+    }
+
+    private void checkInnerMarkup(String text, String markup) {
+        String[] words = text.split("\\s+");
+        for (String word : words) {
+            hasInnerMarkup(word, markup);
+        }
+    }
+
+    private void hasInnerMarkup(String word, String markup) {
+        String result;
+        for (String mark: markups) {
+            if (word.startsWith(markup) ) {
+                result = word.substring(markup.length());
+                if (result.startsWith(mark)) {
+                    throw new IllegalArgumentException("ERROR: nested markup");
+                }
+            }
+            if (word.endsWith(markup)) {
+                result = word.substring(0,word.length()-1);
+                if (result.endsWith(mark)) {
+                    throw new IllegalArgumentException("ERROR: nested markup");
+                }
             }
         }
     }
