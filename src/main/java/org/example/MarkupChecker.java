7f84a60d90f03a6da699076749efad8d5d3a04db
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-03-29 10:54:15 +0200

    add unpaired markup logic

--- src/main/java/org/example/MarkupChecker.java
+++ src/main/java/org/example/MarkupChecker.java
@@ -1,10 +1,9 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import static org.example.MarkdownParser.*;
 
 public class MarkupChecker {
     private final String[] markups = new String[]{"**", "```", "`", "_"};
@@ -21,22 +20,23 @@ public class MarkupChecker {
     }
 
     public void checkUnpairedMarkup(String text) {
-        for (String markup: markups) {
+        checkUnpairedPreformatted(text);
+        for (String markup : markups) {
             checkInnerMarkup(text, markup);
             hasUnpairedMarkup(text, markup);
         }
     }
 
     private void hasUnpairedMarkup(String text, String markup) {
-        text = text.replaceAll(BOLD_REGEX, "BOLD");
-        text = text.replaceAll(ITALIC_REGEX, "ITALIC");
-        text = text.replaceAll(MONOSPACED_REGEX, "MONO");
+        text = text.replaceAll(MarkdownParser.BOLD_REGEX, "BOLD");
+        text = text.replaceAll(MarkdownParser.ITALIC_REGEX, "ITALIC");
+        text = text.replaceAll(MarkdownParser.MONOSPACED_REGEX, "MONO");
 
         String[] words = text.split("\\s+");
         for (String word : words) {
             if (word.startsWith(markup) && !word.endsWith(markup) ||
-                    word.endsWith(markup) && !word.startsWith(markup)) {
-                throw new MarkdownException("Error: unpaired markup");
+                word.endsWith(markup) && !word.startsWith(markup)) {
+                throw new MarkdownException("ERROR: unpaired markup");
             }
         }
     }
@@ -48,18 +48,31 @@ public class MarkupChecker {
         }
     }
 
+    private void checkUnpairedPreformatted(String text) {
+        List<String> preformattedMarkupList = new ArrayList<>();
+        String[] words = text.split("\\s+");
+        for (String word: words) {
+            if (word.equals("```")) {
+                preformattedMarkupList.add(word);
+            }
+        }
+        if (preformattedMarkupList.size() % 2  != 0) {
+            throw new MarkdownException("ERROR: unpaired markup");
+        }
+    }
+
     private void hasInnerMarkup(String word, String markup) {
         String result;
-        String regex = "[A-Za-z0-9,\\u0400-\\u04FF]";
-        for (String mark: markups) {
-            if (word.startsWith(markup) && word.contains(regex)) {
+        String regex = ".*[A-Za-z0-9].*";
+        for (String mark : markups) {
+            if (word.startsWith(markup) && word.matches(regex)) {
                 result = word.substring(markup.length());
                 if (result.startsWith(mark)) {
                     throw new MarkdownException("ERROR: unpaired nested markup");
                 }
             }
-            if (word.endsWith(markup) && word.contains(regex)) {
-                result = word.substring(0,word.length()-1);
+            if (word.endsWith(markup) && word.matches(regex)) {
+                result = word.substring(0, word.length() - markup.length());
                 if (result.endsWith(mark)) {
                     throw new MarkdownException("ERROR: unpaired nested markup");
                 }
